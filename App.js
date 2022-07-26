bcb0386be04c505d35ee8bd058c84a95c6bb0c55
Bruno Goniadis <56612714+Brunogoniadis@users.noreply.github.com>
2022-07-25 22:50:39 -0300

    Função de exclusão de item em lista
    Adição da função de exclusão, com acesso no banco de dados e filtro para manter item

--- App.js
+++ App.js
@@ -77,7 +77,12 @@ export default function App() {
  }
 
  function handleDelete(key){
-  alert(key);
+  firebase.database().ref('tarefas').child(user).child(key).remove()
+  .then(()=>{
+    const findTasks = tasks.filter( item => item.key !== key )
+    setTasks(findTasks)
+  })
+
  } 
 
  function handleEdit(data){
